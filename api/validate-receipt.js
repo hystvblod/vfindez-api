215d0de9d2abf03bd9cc0720697a610c2f8eff15
hystvblod <vboldcompany@gmail.com>
2025-06-13 21:00:10 +0200

    Update validate-receipt.js

--- api/validate-receipt.js
+++ api/validate-receipt.js
@@ -1,9 +1,18 @@
+export const config = {
+  api: {
+    bodyParser: true,
+    externalResolver: true,
+  },
+};
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Autorise la préflight request
+  }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Méthode non autorisée' });
